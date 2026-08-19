51b91711bcfed8d56eaba2903c58b6e68edfd413
Ваше Имя <klypcontacts@gmail.com>
2026-08-19 15:26:40 +0200

    Fix DriverCreationForm inheritance, remove unused imports and fix whitespaces

--- taxi/forms.py
+++ taxi/forms.py
@@ -5,55 +5,94 @@ from taxi.models import Car, Driver
 
 
 class DriverCreationForm(forms.ModelForm):
-    class Meta:
-        model = Driver
-        fields = ("username", "password", "first_name", "last_name", "license_number")
-        widgets = {
-            "password": forms.PasswordInput(),
-        }
+    class Meta:
+        model = Driver
+        fields = ("username", "password", "first_name", "last_name", "license_number")
+        widgets = {
+            "password": forms.PasswordInput(),
+        }
 
-    def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-        if len(license_number) != 8:
-            raise forms.ValidationError("License number must consist of 8 characters.")
-        if not license_number[:3].isupper() or not license_number[:3].isalpha():
-            raise forms.ValidationError("First 3 characters must be uppercase letters.")
-        if not license_number[3:].isdigit():
-            raise forms.ValidationError("Last 5 characters must be digits.")
-        return license_number
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise forms.ValidationError("License number must consist of 8 characters.")
+        if not license_number[:3].isupper() or not license_number[:3].isalpha():
+            raise forms.ValidationError("First 3 characters must be uppercase letters.")
+        if not license_number[3:].isdigit():
+            raise forms.ValidationError("Last 5 characters must be digits.")
+        return license_number
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
 
 
 class DriverLicenseUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Driver
-        fields = ("license_number",)
+    class Meta:
+        model = Driver
+        fields = ("license_number",)
 
-    def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-        if len(license_number) != 8:
-            raise forms.ValidationError("License number must consist of 8 characters.")
-        if not license_number[:3].isupper() or not license_number[:3].isalpha():
-            raise forms.ValidationError("First 3 characters must be uppercase letters.")
-        if not license_number[3:].isdigit():
-            raise forms.ValidationError("Last 5 characters must be digits.")
-        return license_number
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise forms.ValidationError("License number must consist of 8 characters.")
+        if not license_number[:3].isupper() or not license_number[:3].isalpha():
+            raise forms.ValidationError("First 3 characters must be uppercase letters.")
+        if not license_number[3:].isdigit():
+            raise forms.ValidationError("Last 5 characters must be digits.")
+        return license_number
 
 
 class CarForm(forms.ModelForm):
-    drivers = forms.ModelMultipleChoiceField(
-        queryset=get_user_model().objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=False,
-    )
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
 
-    class Meta:
-        model = Car
-        fields = "__all__"
+    class Meta:
+        model = Car
+        fields = "__all__"
 
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from taxi.models import Driver
+
+
+class DriverCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "license_number",
+        )
+
+    def clean_license_number(self) -> str:
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise forms.ValidationError("License number must consist of 8 characters.")
+        if not license_number[:3].isupper() or not license_number[:3].isalpha():
+            raise forms.ValidationError("First 3 characters must be uppercase letters.")
+        if not license_number[3:].isdigit():
+            raise forms.ValidationError("Last 5 characters must be digits.")
+        return license_number
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Driver
+        fields = ("license_number",)
+
+    def clean_license_number(self) -> str:
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise forms.ValidationError("License number must consist of 8 characters.")
+        if not license_number[:3].isupper() or not license_number[:3].isalpha():
+            raise forms.ValidationError("First 3 characters must be uppercase letters.")
+        if not license_number[3:].isdigit():
+            raise forms.ValidationError("Last 5 characters must be digits.")
+        return license_number
--- taxi/views.py
+++ taxi/views.py
@@ -8,91 +8,91 @@ from taxi.forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
 
 
 class DriverListView(generic.ListView):
-    model = Driver
-    paginate_by = 5
+    model = Driver
+    paginate_by = 5
 
 
 class DriverDetailView(generic.DetailView):
-    model = Driver
-    queryset = Driver.objects.prefetch_related("cars__manufacturer")
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
 
 
 class DriverCreateView(generic.CreateView):
-    model = Driver
-    form_class = DriverCreationForm
-    success_url = reverse_lazy("taxi:driver-list")
+    model = Driver
+    form_class = DriverCreationForm
+    success_url = reverse_lazy("taxi:driver-list")
 
 
 class DriverDeleteView(generic.DeleteView):
-    model = Driver
-    success_url = reverse_lazy("taxi:driver-list")
+    model = Driver
+    success_url = reverse_lazy("taxi:driver-list")
 
 
 class DriverLicenseUpdateView(generic.UpdateView):
-    model = Driver
-    form_class = DriverLicenseUpdateForm
-    template_name = "taxi/driver_license_update.html"
+    model = Driver
+    form_class = DriverLicenseUpdateForm
+    template_name = "taxi/driver_license_update.html"
 
-    def get_success_url(self):
-        return reverse_lazy("taxi:driver-detail", kwargs={"pk": self.object.pk})
+    def get_success_url(self):
+        return reverse_lazy("taxi:driver-detail", kwargs={"pk": self.object.pk})
 
 
 class CarListView(generic.ListView):
-    model = Car
-    paginate_by = 5
-    queryset = Car.objects.select_related("manufacturer")
+    model = Car
+    paginate_by = 5
+    queryset = Car.objects.select_related("manufacturer")
 
 
 class CarDetailView(generic.DetailView):
-    model = Car
-    queryset = Car.objects.prefetch_related("drivers")
+    model = Car
+    queryset = Car.objects.prefetch_related("drivers")
 
 
 class CarCreateView(generic.CreateView):
-    model = Car
-    form_class = CarForm
-    success_url = reverse_lazy("taxi:car-list")
+    model = Car
+    form_class = CarForm
+    success_url = reverse_lazy("taxi:car-list")
 
 
 class CarUpdateView(generic.UpdateView):
-    model = Car
-    form_class = CarForm
-    success_url = reverse_lazy("taxi:car-list")
+    model = Car
+    form_class = CarForm
+    success_url = reverse_lazy("taxi:car-list")
 
 
 class CarDeleteView(generic.DeleteView):
-    model = Car
-    success_url = reverse_lazy("taxi:car-list")
+    model = Car
+    success_url = reverse_lazy("taxi:car-list")
 
 
 class ManufacturerListView(generic.ListView):
-    model = Manufacturer
-    paginate_by = 5
+    model = Manufacturer
+    paginate_by = 5
 
 
 class ManufacturerCreateView(generic.CreateView):
-    model = Manufacturer
-    fields = "__all__"
-    success_url = reverse_lazy("taxi:manufacturer-list")
+    model = Manufacturer
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
 
 
 class ManufacturerUpdateView(generic.UpdateView):
-    model = Manufacturer
-    fields = "__all__"
-    success_url = reverse_lazy("taxi:manufacturer-list")
+    model = Manufacturer
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
 
 
 class ManufacturerDeleteView(generic.DeleteView):
-    model = Manufacturer
-    success_url = reverse_lazy("taxi:manufacturer-list")
+    model = Manufacturer
+    success_url = reverse_lazy("taxi:manufacturer-list")
 
 
 @login_required
 def toggle_car_driver(request, pk):
-    car = get_object_or_404(Car, pk=pk)
-    if request.user in car.drivers.all():
-        car.drivers.remove(request.user)
-    else:
-        car.drivers.add(request.user)
-    return HttpResponseRedirect(reverse_lazy("taxi:car-detail", kwargs={"pk": pk}))
+    car = get_object_or_404(Car, pk=pk)
+    if request.user in car.drivers.all():
+        car.drivers.remove(request.user)
+    else:
+        car.drivers.add(request.user)
+    return HttpResponseRedirect(reverse_lazy("taxi:car-detail", kwargs={"pk": pk}))
 
