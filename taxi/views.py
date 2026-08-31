cfc942c3a62bd5e4a70684d0598f06287458eaa9
Ваше Имя <klypcontacts@gmail.com>
2026-08-31 14:21:26 +0200

    Fix remaining flake8 linting errors and file endings

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.decorators import login_required
+﻿from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
