a576b9aa64058b31995909a14c7cfa51b67c63df
Ваше Имя <klypcontacts@gmail.com>
2026-08-31 14:12:54 +0200

    Add index view function

--- taxi/views.py
+++ taxi/views.py
@@ -99,3 +99,17 @@ def toggle_car_driver(request, pk):
     return HttpResponseRedirect(
         reverse_lazy("taxi:car-detail", kwargs={"pk": pk})
     )
+
+
+def index(request):
+    num_drivers = Driver.objects.count()
+    num_cars = Car.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+
+    context = {
+        "num_drivers": num_drivers,
+        "num_cars": num_cars,
+        "num_manufacturers": num_manufacturers,
+    }
+
+    return render(request, "taxi/index.html", context=context)
