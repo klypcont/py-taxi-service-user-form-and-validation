1ac7547ef405ce8456373cc897d47a840d161040
Ваше Имя <klypcontacts@gmail.com>
2026-08-31 14:18:11 +0200

    Fix urls configuration and formatting

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+﻿from django.urls import path
 
 from taxi.views import (
     index,
@@ -46,12 +46,24 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("cars/<int:pk>/toggle-driver/", toggle_car_driver, name="toggle-car-driver"),
+    path(
+        "cars/<int:pk>/toggle-driver/",
+        toggle_car_driver,
+        name="toggle-car-driver",
+    ),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
-    path("drivers/<int:pk>/update/", DriverLicenseUpdateView.as_view(), name="driver-update"),
-    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
+    path(
+        "drivers/<int:pk>/update/",
+        DriverLicenseUpdateView.as_view(),
+        name="driver-update",
+    ),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete",
+    ),
 ]
 
 app_name = "taxi"
