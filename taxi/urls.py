02014bbbeff288b185c7ee3c71c132924d97c5a4
Ваше Имя <klypcontacts@gmail.com>
2026-08-31 14:23:51 +0200

    Fix final E501 line length in urls.py

--- taxi/urls.py
+++ taxi/urls.py
@@ -52,8 +52,16 @@ urlpatterns = [
         name="toggle-car-driver",
     ),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
-    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail",
+    ),
+    path(
+        "drivers/create/",
+        DriverCreateView.as_view(),
+        name="driver-create",
+    ),
     path(
         "drivers/<int:pk>/update/",
         DriverLicenseUpdateView.as_view(),
